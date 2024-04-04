81cace04e83b485291b43e0418a483307feef88a
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-04 14:57:10 -0300

    Consulta ao banco com INNER JOIN com SQL

--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2621.entities.Product;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+
+    @Query(nativeQuery = true,
+            value = "SELECT products.name FROM products " +
+            "INNER JOIN providers ON providers.id = products.id_providers " +
+            "WHERE providers.name LIKE CONCAT(:name, '%') " +
+            "AND products.amount BETWEEN :min AND :max")
+    List<ProductMinProjection> findProductByProducerAndQuantitySQL(String name, Integer min, Integer max);
 }
