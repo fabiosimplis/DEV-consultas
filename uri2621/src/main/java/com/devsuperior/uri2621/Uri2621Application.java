7379749ee8dfa681b5fb1cee7144dcdbb873ff03
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-04 15:18:15 -0300

    Consulta ao banco com INNER JOIN com JPQL

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -31,5 +31,14 @@ public class Uri2621Application implements CommandLineRunner {
 		for (ProductMinDTO pDto : listProductDTO){
 			System.out.println(pDto);
 		}
+
+		System.out.println("\n\n");
+
+		List<ProductMinDTO> listProductJPQL = repository.findProductByProducerAndQuantityJPQL("P", 10, 20);
+
+		System.out.println("\n*** JPQL ***");
+		for (ProductMinDTO pDto : listProductJPQL){
+			System.out.println(pDto);
+		}
 	}
 }
--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -17,4 +18,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "WHERE providers.name LIKE CONCAT(:name, '%') " +
             "AND products.amount BETWEEN :min AND :max")
     List<ProductMinProjection> findProductByProducerAndQuantitySQL(String name, Integer min, Integer max);
+
+    @Query("SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(p.name) " +
+            "FROM Product p " +
+                    "WHERE p.provider.name LIKE CONCAT(:name, '%') " +
+                    "AND p.amount BETWEEN :min AND :max")
+    List<ProductMinDTO> findProductByProducerAndQuantityJPQL(String name, Integer min, Integer max);
 }
