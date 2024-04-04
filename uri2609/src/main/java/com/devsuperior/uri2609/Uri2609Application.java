df3a081fdc18008ebe930ed8f36db14ea26f23ae
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-04 16:41:16 -0300

    Consulta usando SUM e GROUP by SQL

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -19,6 +24,12 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		List<CategorySumProjection> groupSumList = repository.groupQuantityByCategory();
+		List<CategorySumDTO> categorySumList = groupSumList.stream().map(CategorySumDTO::new).collect(Collectors.toList());
 
+		System.out.println("\n*** SQL ***");
+		for (CategorySumDTO cDto : categorySumList){
+			System.out.println(cDto);
+		}
 	}
 }
--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.uri2609.repositories;
 
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2609.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query(nativeQuery = true,
+            value = "SELECT c.name, SUM(p.amount) " +
+                    "FROM products AS p " +
+                    "INNER JOIN categories AS c " +
+                    "ON p.id_categories = c.id " +
+                    "GROUP BY c.name")
+    List<CategorySumProjection> groupQuantityByCategory();
 
 }
