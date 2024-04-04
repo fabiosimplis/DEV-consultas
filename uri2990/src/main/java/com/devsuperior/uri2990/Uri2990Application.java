f81f4b86664a6dd4b66bc40f6e97fb20e94ac9e1
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-04 19:36:33 -0300

    Consulta usando difeença/left join

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +24,21 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		List<EmpregadoDeptProjection> listProj = repository.searchSQL();
+		List<EmpregadoDeptDTO> listDTO = listProj.stream().map(EmpregadoDeptDTO::new).collect(Collectors.toList());
+
+		System.out.println("\n**** SQL ****");
+		for (EmpregadoDeptDTO empDto : listDTO){
+			System.out.println(empDto);
+		}
+		System.out.println("\n\n");
+
+		List<EmpregadoDeptDTO> listDTOJPQL = repository.searchJPQL();
+
+		System.out.println("\n**** JPQL ****");
+		for (EmpregadoDeptDTO empDto : listDTOJPQL){
+			System.out.println(empDto);
+		}
+		System.out.println("\n\n");
 	}
 }
--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,40 @@
 package com.devsuperior.uri2990.repositories;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2990.entities.Empregado;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
 
+    @Query(nativeQuery = true, value =
+            "SELECT empregados.cpf, empregados.enome, departamentos.dnome " +
+            "FROM empregados " +
+            "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero " +
+            "WHERE empregados.cpf NOT IN ( " +
+            " SELECT trabalha.cpf_emp " +
+            " FROM trabalha) " +
+            "ORDER BY empregados.cpf")
+    List<EmpregadoDeptProjection> searchSQL();
+
+
+   /*@Query("SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(e.cpf, e.enome, e.departamento.dnome) " +
+                    "FROM Empregado e " +
+                    "WHERE e.cpf NOT IN ( " +
+                    " SELECT e.projetosOndeTrabalha FROM Empregado e " +
+                    ") " +
+                    "ORDER BY e.cpf")*/
+   @Query("SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(e.cpf, e.enome, e.departamento.dnome) " +
+           "FROM Empregado e " +
+           "WHERE e.cpf NOT IN ( " +
+           " SELECT e.cpf " +
+           " FROM Empregado e " +
+           " INNER JOIN e.projetosOndeTrabalha " +
+           ") " +
+           "ORDER BY e.cpf")
+    List<EmpregadoDeptDTO> searchJPQL();
 }
